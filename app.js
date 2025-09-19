58a598d370b7c162b55433a320b0d000ae2d1ed6
Your Name~ <aamodmishra111@gmail.com>
2025-09-19 23:09:12 +0545

    new commit

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-//
+// creating a collection name employee 
+
+var {MongoClient} = require ('mongodb');
+var url = "mongodb://localhost:27017/";
+ // now connect mongodb server using mongoclient 
+ const client =new MongoClient(url);
+
+async function run(){
+    try{
+      await client.connect();
+      const db =client.db ("mydb");
+
+      await db.createCollection('employee');
+      console.log("collection created");
+
+      
+    }catch (err){
+        console.error("error",err)
+    }finally {
+        await client.close();
+    }
+}
+
+run();
