b56224c383d527e09b6e3385e0292c3ce9f27736
Your Name~ <aamodmishra111@gmail.com>
2025-09-21 14:40:55 +0545

    added file to delete  data

---
+++ inserting.js
@@ -0,0 +1,42 @@
+const { MongoClient } = require("mongodb");
+
+// 1. Connection URL
+const url = "mongodb://localhost:27017";  
+const client = new MongoClient(url);
+
+async function run() {
+  try {
+    // Connect to MongoDB server
+    await client.connect();
+    console.log("✅ Connected to MongoDB");
+
+    //  Select (or create) a database
+    const db = client.db("mydb");
+
+    //  Create a collection named "employee"
+    await db.createCollection("employee");
+    console.log("📂 Collection 'employee' created!");
+
+    // 5. Insert employee data into collection
+    const employeeData = [
+      { name: "Aamod", role: "Software Engineer", salary: 50000 },
+      { name: "Ramesh", role: "Tester", salary: 30000 },
+      { name: "Sita", role: "Manager", salary: 70000 }
+    ];
+
+    const result = await db.collection("employee").insertMany(employeeData);
+    console.log(`✅ ${result.insertedCount} employees inserted!`);
+
+    const employee =await employeeCollection.find().toarray();
+    console.log("emplayee:",employee)
+
+  } catch (err) {
+    console.error("❌ Error:", err);
+  } finally {
+    // 6. Close the connection
+    await client.close();
+    console.log("🔒 Connection closed");
+  }
+}
+
+run();
